3c3ae4264b77a1bdadb43e7ab7be4b1069dfca54
VitalyTaran <79220120+VitalyTaran@users.noreply.github.com>
2023-01-19 20:44:25 +0300

    Initial Commit

---
+++ WeatherAppExample/WeatherAppExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppExampleApp.swift
+//  WeatherAppExample
+//
+//  Created by Виталий Таран on 19.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
